49c7e8cef3f091df8f76fb0899ecd8da2f640c90
Brian <brian@briankiernan.com>
2024-08-08 15:20:45 -0400

    add channel example

--- multifunc/main.go
+++ multifunc/main.go
@@ -5,28 +5,33 @@ import (
 	"sync"
 )
 
-func generateNumbers(total int, wg *sync.WaitGroup) {
+func generateNumbers(total int, ch chan<- int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for idx := 1; idx <= total; idx++ {
-		fmt.Printf("Generating number %d\n", idx)
+		fmt.Printf("sending %d to channel\n", idx)
+		ch <- idx
 	}
 }
 
-func printNumbers(wg *sync.WaitGroup) {
+func printNumbers(ch <-chan int, wg *sync.WaitGroup) {
 	defer wg.Done()
 
-	for idx := 1; idx <= 3; idx++ {
-		fmt.Printf("Printing number %d\n", idx)
+	for num := range ch {
+		fmt.Printf("read %d from channel\n", num)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	numberChan := make(chan int)
 
 	wg.Add(2)
-	go printNumbers(&wg)
-	go generateNumbers(3, &wg)
+	go printNumbers(numberChan, &wg)
+
+	generateNumbers(3, numberChan, &wg)
+
+	close(numberChan)
 
 	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
